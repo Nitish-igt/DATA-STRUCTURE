18697760b62b721878d3dd30654ba441ec703530
Nitish kumar <nitishtty@gmail.com>
2025-09-17 13:05:37 +0530

    Add files via upload

---
+++ 1910 leetcode.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+void removeocc(string &s, string &part){
+    int found=s.find(part);
+    if(found != string::npos){
+        string left_part=s.substr(0,found);
+        string right_part=s.substr(found+part.size(),s.size());
+       s=left_part+right_part;
+
+        removeocc(s,part);
+    }
+    else{
+        // base case 
+        // all the occurence removed form 
+          return;
+    }
+}
+    string removeOccurrences(string s, string part) {
+       removeocc(s, part);
+       return s;
+    }
+};
